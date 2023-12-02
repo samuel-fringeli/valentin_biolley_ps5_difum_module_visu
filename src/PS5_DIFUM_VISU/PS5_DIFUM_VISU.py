4a9bfa3aeb59bc6ee8bde165a150f0c36f6efcd6
valentin.biolley <valentin.biolley@edu.hefr.ch>
2023-12-02 16:07:05 +0100

    Add restructured module new version

--- src/PS5_DIFUM_VISU/PS5_DIFUM_VISU.py
+++ src/PS5_DIFUM_VISU/PS5_DIFUM_VISU.py
@@ -1,3 +1,12 @@
+"""
+PS5_DIFUM_VISU
+
+Module of Visualisation of embedding vectors
+
+Autor : Biolley
+Email : valentin.biolley@edu.hefr.ch
+Date : 01.12.23
+"""
 import numpy as np
 import torch
 from sklearn.manifold import TSNE
@@ -9,7 +18,7 @@ from PIL import Image
 import os
 
 
-def tokenizerLabels(labels, tokenizer):
+def tokenizerLabels (labels,tokenizer):
     """Tokenize the given labels
 
     Args:
@@ -19,34 +28,36 @@ def tokenizerLabels(labels, tokenizer):
     Returns:
         list: The list of tokens
     """
-    resArray = []
+    resArray =[]
     for label in labels:
         # Tokenization
         tokens = tokenizer(label, return_tensors="pt")
         resArray.append(tokenizer.convert_ids_to_tokens(tokens["input_ids"].squeeze().tolist()))
     return resArray
 
-
-def get_txt_embedding(labels, model, tokenizer):
+def get_txt_embedding_bert(labels,model,tokenizer):
     """Computes the embeddings for the given labels
 
     Args:
         labels(list[str]): The labels to encode
+        model(object): Text encoder model
+        tokenizer(object): Model tokenizer
 
     Returns:
         tensor: The tensor of encoded labels
     """
+    # Tokenization of labels
     tokens = tokenizer(labels, return_tensors="pt", padding=True, truncation=True)
     # Obtaining the embeddings
     outputs = model(**tokens)
+    # Get the last_hidden_state
     embeddings = outputs.last_hidden_state
 
     return embeddings
 
-
-def compute_cosine_similarities(embeddings_1, ref_label_idx=0, embeddings_2=None):
+def compute_cosine_similarities(embeddings_1,ref_label_idx = 0,embeddings_2=None):
     """
-    Compute similarities between a reference label and a list of labels using text embeddings.
+    Compute similarities between a reference label and a list of labels using embeddings (txt or images).
 
     Parameters:
         embeddings_1 (tensor): A tensor for which you want to calculate similarities
@@ -54,24 +65,30 @@ def compute_cosine_similarities(embeddings_1, ref_label_idx=0, embeddings_2=None
         embeddings_2 (tensor,optional): The tensor who contains the ref_label_idx (if not here, use ref_label of Labels1 )
 
     Returns:
+        Raise ValueError: If tensors haven't the same size
         similarities (numpy.ndarray): An array of similarities corresponding to the labels.
     """
+    # Create pythorch cosine similarity instance, use for compute the cosine similarity
     cos = torch.nn.CosineSimilarity(dim=-1)
-    if embeddings_2 is None:
-        embeddings_2 = embeddings_1
+    # Check if optional parameter embeddings_2 is given
+    if  embeddings_2 is None:
+        # If not compute cosine similarity between embedding_1 and embedding_1
+        embeddings_2=embeddings_1
+    # If tensors are not the same size, throw an exception
     if embeddings_1.size() != embeddings_2.size():
-        raise ValueError("Size of embeddings 1 (", embeddings_1.shape, ") and size of embedding2(", embeddings_2.shape,
-                         ") is not equals")
-    embeddings_1 = embeddings_1.view(embeddings_1.size(0), -1)
-    embeddings_2 = embeddings_2.view(embeddings_2.size(0), -1)
+        raise ValueError("Size of embeddings 1 (",embeddings_1.shape,") and size of embedding2(",embeddings_2.shape,") is not equals")
+    # reshaping tensors without losing information
+    embeddings_1=embeddings_1.view(embeddings_1.size(0),-1)
+    embeddings_2=embeddings_2.view(embeddings_2.size(0),-1)
 
-    similarities = np.array(
-        [cos(embeddings_2[ref_label_idx], embeddings_1[i]).detach().numpy() for i in range(len(embeddings_1))])
+    # Compute cosine similarities between each embedding vector and the ref_label_idx embedding vector
+    similarities = np.array([cos(embeddings_2[ref_label_idx], embeddings_1[i]).detach().numpy() for i in range(len(embeddings_1))])
 
     return similarities
 
 
-def get_TSNE(embeddings, component=2):
+
+def get_TSNE (embeddings,component=2):
     """
     Compute the TSNE of a tensor, reduce the dimension to n_component
 
@@ -81,14 +98,17 @@ def get_TSNE(embeddings, component=2):
     Returns:
         text_embeddings_TSNE (numpy.ndarray nD): An array nD who represente the tensor.
     """
-    embeddings = embeddings.view(embeddings.size(0), -1)
-    embeddings = embeddings.detach().numpy()
-    tsne = TSNE(random_state=1, n_components=component, metric="cosine", perplexity=2.0)
-    embeddings = tsne.fit_transform(embeddings)
+    # reshaping tensors without losing information
+    embeddings = embeddings.view(embeddings.size(0),-1)
+    # Transforme tensor to numpy array
+    embeddings=embeddings.detach().numpy()
+    # Create sklearn TSNE instance
+    tsne = TSNE(random_state=1,n_components=component,metric="cosine",perplexity=2)
+    # Apply TSNE
+    embeddings =tsne.fit_transform(embeddings)
     return embeddings
 
-
-def get_PCA(embeddings, component=2):
+def get_PCA (embeddings,component=2):
     """
     Compute the PCA of a tensor, reduce the dimension to n_component.
 
@@ -98,14 +118,17 @@ def get_PCA(embeddings, component=2):
     Returns:
         text_embeddings_PCA (numpy.ndarray nD): An array nD who represent the tensor.
     """
-    embeddings = embeddings.reshape(embeddings.shape[0], -1)
-    embeddings = embeddings.detach().numpy()
+    # reshaping tensors without losing information
+    embeddings = embeddings.view(embeddings.size(0),-1)
+    # Transforme tensor to numpy array
+    embeddings=embeddings.detach().numpy()
+    # Create sklearn PCA instance
     pca = PCA(n_components=component)
-    embeddings = pca.fit_transform(embeddings)
+    # Apply PCA
+    embeddings =pca.fit_transform(embeddings)
     return embeddings
 
-
-def get_img_embedding_swin(urls, model, image_processor):
+def get_img_embedding_swin (urls,model,image_processor):
     """Computes the embeddings for the given image locate in urls
 
     Args:
@@ -123,7 +146,7 @@ def get_img_embedding_swin(urls, model, image_processor):
             image = image.convert('RGB')
             new_image_path = os.path.splitext(image_path)[0] + ".jpg"
             image.save(new_image_path)
-            image = Image.open(new_image_path)
+            image=Image.open(new_image_path)
         else:
             image = Image.open(image_path)
 
@@ -137,8 +160,7 @@ def get_img_embedding_swin(urls, model, image_processor):
     tensor_embeddings = torch.stack(embeddings_img)
     return tensor_embeddings
 
-
-def read_file_label(url_file):
+def read_file_label (url_file):
     """Read label in a file
 
     Args:
@@ -147,29 +169,30 @@ def read_file_label(url_file):
     Returns:
         lines(list[str]): The list of labels
     """
-    with open(url_file, 'r') as f:
+    # Open file and return each line in an array
+    with open(url_file,'r') as f:
         lines = f.read().splitlines()
         return lines
 
-
 def read_dir_image(url_dir):
-    """Read label in a file
+    """Read images present in directory and build urls to access all images
 
     Args:
         url_dir(list[str]): The url of directory containing the images
 
     Returns:
-        res(list[str]): The list of images
+        res(list[str]): The list of urls images contain in the directory
     """
-    res = []
+    res=[]
+    # Enumerate all file present in the directory
     files = os.listdir(url_dir)
+    # For each file, build url to access all images
     for file in files:
-        final_url = url_dir + file
+        final_url=url_dir+file
         res.append(final_url)
     return res
 
-
-def save_tensor(embedding, label, path):
+def save_tensor(embedding,label,path):
     """Save a tensor on /path_label.pt on disk
 
     Args:
@@ -180,80 +203,81 @@ def save_tensor(embedding, label, path):
     Returns:
         -
     """
-    torch.save(embedding, path + "_" + label + ".pt")
+    torch.save(embedding,path+"_"+label+".pt")
 
-
-def load_tensor(path, label):
+def load_tensor(path,label):
     """Load tensor from disk
 
     Args:
-        path(str): location where to load the tensor
+        path(str): Location where to load the tensor
+        label(str): Load only tensor that match with label
 
     Returns:
         tensor(tensor): Tensor loaded
         label(str): Label of loaded tensor
     """
+    # Enumerate all file present in the directory
     files = os.listdir(path)
-    labels = []
-    tensors = []
+    labels=[]
+    tensors=[]
+    # for each file get only file that match the label
     for file in files:
-        file_name = os.path.splitext(os.path.basename(path + file))[0]
-        label_split = file_name.split('_', 1)
-        if label_split[1] == label:
+        file_name = os.path.splitext(os.path.basename(path+file))[0]
+        label_split=file_name.split('_', 1)
+        if label_split[1]==label:
             labels.append(label_split[1])
-            tensors.append(torch.load(path + "/" + file))
+            tensors.append(torch.load(path+"/"+file))
 
-    return tensors, labels
+    return tensors,labels
 
-
-def create_data_set_for_vis(embeddings, embeddings_2, input_name, label, ref_label_idx=0):
-    """Computes the dataframe to help the visualisation with plotly
+def create_data_set_for_vis(embeddings,input_name,label=0, ref_label_idx=0):
+    """Computes the dataframe to help the visualisation with plotly, use the return of this function as input of the fonction visualise_embedding
 
     Args:
-        embeddings(tensor): first tensor to display
-        embeddings_2(tensor): Second tensor to display
-        input_name(list[string): List of name of embeddings use for display name of points
-        label(list[string]): Labels use for display the color of points
+        embeddings(list[tensor]): list of tensors to display
+        input_name(list[string): List of name of embeddings use for display name of points (same size of embeddings)
+        label(list[string],optional): Labels use for display the color of points (same size of embeddings)
         ref_label_idx (int, optional): The index of the reference label for which to calculate similarities. Default is 0
-
-
     Returns:
+        Raise ValueError: If embeddings,input_name and label haven't the same size
         dataframe: The dataFrame of all data we need to create graph
     """
-    similarity = compute_cosine_similarities(embeddings, ref_label_idx)
-    similarity2 = compute_cosine_similarities(embeddings, ref_label_idx, embeddings_2)
+    # Check size of embeddinds,input_name,label, if not equals raise an error
+    if len(embeddings)!=len(input_name):
+        raise ValueError("Size of embeddings (", len(embeddings),") and size of prompt(", len(input_name),") is not equals")
+    if label!=0:
+        if len(embeddings)!=len(labels):
+            raise ValueError("Size of embeddings (", len(embeddings),") and size of label(", len(label),") is not equals")
+    # Create empty dataFrame for result
+    data_to_vis=pd.DataFrame()
+    i=0
+    #For each embedding compute all necessary data for visualisation
+    for embedding in embeddings:
+        data_to_vis_temp = pd.DataFrame()
+        #compute cosine similarity
+        similarity=compute_cosine_similarities(embeddings[ref_label_idx], ref_label_idx,embedding)
+        #compute TSNE
+        txt_embedding_tsne = get_TSNE(embedding)
+        txt_embedding_tsne_3_componnent= get_TSNE(embedding,3)
+        #add all this data on the dataFrame
+        data_to_vis_temp["embedding_X"]=txt_embedding_tsne[:,0]
+        data_to_vis_temp["embedding_Y"]=txt_embedding_tsne[:,1]
+        data_to_vis_temp["embedding_X_3D"]=txt_embedding_tsne_3_componnent[:,0]
+        data_to_vis_temp["embedding_y_3D"]=txt_embedding_tsne_3_componnent[:,1]
+        data_to_vis_temp["embedding_z_3D"]=txt_embedding_tsne_3_componnent[:,2]
+        data_to_vis_temp["prompt"]=input_name[i]
+        data_to_vis_temp["similarity"]=similarity
+        #if no label is given
+        if label!=0:
+            data_to_vis_temp["label"]=label[i]
+        #concate this dataFrame to dataframe for result
+        data_to_vis=pd.concat([data_to_vis,data_to_vis_temp],ignore_index=True)
+        i+=1
 
-    data_to_vis1 = pd.DataFrame()
-    txt_embedding_tsne = get_TSNE(embeddings)
-    txt_embedding_tsne_3_componnent = get_TSNE(embeddings, 3)
-
-    data_to_vis1["embedding_X"] = txt_embedding_tsne[:, 0]
-    data_to_vis1["embedding_Y"] = txt_embedding_tsne[:, 1]
-    data_to_vis1["embedding_X_3D"] = txt_embedding_tsne_3_componnent[:, 0]
-    data_to_vis1["embedding_y_3D"] = txt_embedding_tsne_3_componnent[:, 1]
-    data_to_vis1["embedding_z_3D"] = txt_embedding_tsne_3_componnent[:, 2]
-    data_to_vis1["prompt"] = input_name[0]
-    data_to_vis1["similarity"] = similarity
-    data_to_vis1["label"] = label[0]
-
-    data_to_vis2 = pd.DataFrame()
-    txt_embedding_tsne = get_TSNE(embeddings_2)
-    txt_embedding_tsne_3_componnent = get_TSNE(embeddings_2, 3)
-
-    data_to_vis2["embedding_X"] = txt_embedding_tsne[:, 0]
-    data_to_vis2["embedding_Y"] = txt_embedding_tsne[:, 1]
-    data_to_vis2["embedding_X_3D"] = txt_embedding_tsne_3_componnent[:, 0]
-    data_to_vis2["embedding_y_3D"] = txt_embedding_tsne_3_componnent[:, 1]
-    data_to_vis2["embedding_z_3D"] = txt_embedding_tsne_3_componnent[:, 2]
-    data_to_vis2["prompt"] = input_name[1]
-    data_to_vis2["similarity"] = similarity2
-    data_to_vis2["label"] = label[1]
-
-    data_to_vis = pd.concat([data_to_vis1, data_to_vis2], ignore_index=True)
     return data_to_vis
 
 
-def visualise_embedding(data, ref_label_idx=0):
+def visualise_embedding (data,ref_label_idx=0):
     """Computes the graphe and display it
         - 2D graph with embeddings and 2 components TSNE
         - 3D graph with embeddings and 2 components TSNE and cosine similarity on axe z
@@ -265,40 +289,49 @@ def visualise_embedding(data, ref_label_idx=0):
     Returns:
         -
     """
-    fig = px.scatter(data, x='embedding_X', y='embedding_Y', color='label', hover_name="prompt")
+    # check if label exist in the data
+    if 'label' in data:
+        # display color according to label
+        fig = px.scatter(data, x='embedding_X', y='embedding_Y', color='label', hover_name="prompt")
+    else:
+        # don't display color
+        fig = px.scatter(data, x='embedding_X', y='embedding_Y', hover_name="prompt")
 
     highlight = ref_label_idx
-
+    # get ref_label_idx sample
     x_highlight = data.loc[highlight, 'embedding_X']
     y_highlight = data.loc[highlight, 'embedding_Y']
-
-    fig.add_trace(go.Scatter(x=[x_highlight], y=[y_highlight], mode='markers', marker=dict(size=10, color='green'),
-                             name="input de base: " + data["prompt"][ref_label_idx], visible="legendonly"))
-
-    fig.update_layout(title='Representation vecteurs embedding 2D avec TSNE 2 composantes',
-                      xaxis=dict(title='embedding_X'), yaxis=dict(title='embedding_Y'))
+    # Display this sample in green
+    fig.add_trace(go.Scatter(x=[x_highlight], y=[y_highlight], mode='markers', marker=dict(size=10, color='green'),name="input de base: "+data["prompt"][ref_label_idx],visible="legendonly"))
+    # Add title on the graphe and name axes
+    fig.update_layout(title='Representation vecteurs embedding 2D avec TSNE 2 composantes',xaxis=dict(title='embedding_X'),yaxis=dict(title='embedding_Y'))
 
     fig.show()
-
-    fig = px.scatter_3d(data, x='embedding_X', y='embedding_Y', z='similarity', color='label', hover_name="prompt")
+     # check if label exist in the data
+    if 'label' in data:
+        # display color according to label
+        fig = px.scatter_3d(data, x='embedding_X', y='embedding_Y', z='similarity', color='label', hover_name="prompt")
+    else:
+         # don't display color
+        fig = px.scatter_3d(data, x='embedding_X', y='embedding_Y', z='similarity', hover_name="prompt")
 
     highlight = ref_label_idx
-
+    # get ref_label_idx sample
     x_highlight = data.loc[highlight, 'embedding_X']
     y_highlight = data.loc[highlight, 'embedding_Y']
     z_highlight = data.loc[highlight, 'similarity']
-
-    fig.add_trace(go.Scatter3d(x=[x_highlight], y=[y_highlight], z=[z_highlight], mode='markers',
-                               marker=dict(size=10, color='green'),
-                               name="input de base: " + data["prompt"][ref_label_idx], visible="legendonly"))
-
-    fig.update_layout(
-        title='Representation vecteurs embedding 3D avec TSNE 2 composante et avec la similaritié sur l\'axe z',
-        scene=dict(xaxis=dict(title='embedding_X'), yaxis=dict(title='embedding_Y'), zaxis=dict(title='Similarity')))
+    # Display this sample in green
+    fig.add_trace(go.Scatter3d(x=[x_highlight], y=[y_highlight], z=[z_highlight],mode='markers', marker=dict(size=10, color='green'),name="input de base: "+data["prompt"][ref_label_idx],visible="legendonly"))
+    # Add title on the graphe and name axes
+    fig.update_layout(title='Representation vecteurs embedding 3D avec TSNE 2 composante et avec la similaritié sur l\'axe z',scene=dict(xaxis=dict(title='embedding_X'),yaxis=dict(title='embedding_Y'),zaxis=dict(title='Similarity')))
     fig.show()
-
-    fig = px.scatter_3d(data, x='embedding_X_3D', y='embedding_y_3D', z='embedding_z_3D', color='label',
-                        hover_name="prompt")
+     # check if label exist in the data
+    if 'label' in data:
+        # display color according to label
+        fig = px.scatter_3d(data, x='embedding_X_3D', y='embedding_y_3D', z='embedding_z_3D', color='label', hover_name="prompt")
+    else:
+         # don't display color
+         fig = px.scatter_3d(data, x='embedding_X_3D', y='embedding_y_3D', z='embedding_z_3D', hover_name="prompt")
 
     highlight = ref_label_idx
 
@@ -306,11 +339,8 @@ def visualise_embedding(data, ref_label_idx=0):
     y_highlight = data.loc[highlight, 'embedding_y_3D']
     z_highlight = data.loc[highlight, 'embedding_z_3D']
 
-    fig.add_trace(go.Scatter3d(x=[x_highlight], y=[y_highlight], z=[z_highlight], mode='markers',
-                               marker=dict(size=10, color='green'),
-                               name="input de base: " + data["prompt"][ref_label_idx], visible="legendonly"))
+    fig.add_trace(go.Scatter3d(x=[x_highlight], y=[y_highlight], z=[z_highlight],mode='markers', marker=dict(size=10, color='green'),name="input de base: "+data["prompt"][ref_label_idx],visible="legendonly"))
 
-    fig.update_layout(title='Representation vecteurs embedding 3D avec TSNE 3 composantes',
-                      scene=dict(xaxis=dict(title='embedding_X'), yaxis=dict(title='embedding_Y'),
-                                 zaxis=dict(title='embedding_Z')))
+    fig.update_layout(title='Representation vecteurs embedding 3D avec TSNE 3 composantes',scene=dict(xaxis=dict(title='embedding_X'),yaxis=dict(title='embedding_Y'),zaxis=dict(title='embedding_Z')))
     fig.show()
+
